1a3e1c68e01d26cd0529feacbc77e7947866b667
prestosilver <prestosilver.ptp@gmail.com>
2021-10-07 09:40:42 -0400

    build stuffs

---
+++ Assets/SuperUnityBuild/BuildConstants.cs
@@ -0,0 +1,38 @@
+// This file is auto-generated. Do not modify or move this file.
+
+public static class BuildConstants
+{
+    public enum ReleaseType
+    {
+        None,
+        Release,
+        Devel,
+    }
+
+    public enum Platform
+    {
+        None,
+        Linux,
+        PC,
+    }
+
+    public enum Architecture
+    {
+        None,
+        Linux_x64,
+        Windows_x86,
+    }
+
+    public enum Distribution
+    {
+        None,
+    }
+
+    public static readonly System.DateTime buildDate = new System.DateTime(637691531350892640);
+    public const string version = "1.21.4-8";
+    public const ReleaseType releaseType = ReleaseType.Release;
+    public const Platform platform = Platform.Linux;
+    public const Architecture architecture = Architecture.Linux_x64;
+    public const Distribution distribution = Distribution.None;
+}
+
