8acce2f05702cadff60a77a43119459fbd4f48d9
prestosilver <prestosilver.ptp@gmail.com>
2021-10-11 09:10:38 -0400

    comment menu controller

--- Assets/Scripts/Menu/MenuController.cs
+++ Assets/Scripts/Menu/MenuController.cs
@@ -6,21 +6,59 @@ using UnityEngine.UI;
 
 public class MenuController : MonoBehaviour
 {
-    const string VersionPrefix = "Release";
+    /// <summary>
+    /// the version info prefix
+    /// </summary>
+    const string VersionPrefix = "R";
+
+    /// <summary>
+    /// the text displaying the version
+    /// </summary>
     public Text vtext;
+
+    /// <summary>
+    /// the menu animator instance
+    /// </summary>
     public MenuAnimation ma;
+
+    /// <summary>
+    /// the transition animator
+    /// </summary>
     public Animator transition;
+
+    /// <summary>
+    /// the changelog prefab
+    /// </summary>
     public GameObject ChangeLog;
+
+    /// <summary>
+    /// the add mod from github popup
+    /// </summary>
     public GameObject GHPopup;
+
+    /// <summary>
+    /// the object foreground
+    /// </summary>
     public Transform Foreground;
 
+    /// <summary>
+    /// setup the menu
+    /// </summary>
     public void Start()
     {
+        // setup frame rate
+        Application.targetFrameRate = 60;
+
+        // gpgs login
         PlayGamesScript.SignIn();
+
+        // setup version text
         vtext.text = VersionPrefix + "-" + Application.version;
+
         // Disable screen dimming
         Screen.sleepTimeout = SleepTimeout.NeverSleep;
-        Application.targetFrameRate = 60;
+
+        // show change log on new save / version
         if (PlayerPrefs.GetString("Version") != VersionPrefix + "-" + Application.version)
         {
             Instantiate(ChangeLog);
@@ -28,70 +66,90 @@ public class MenuController : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// show the save select menu
+    /// </summary>
     public void ContinueGame()
     {
         StartCoroutine(LoadScene("SaveSelect"));
     }
 
+    /// <summary>
+    /// show the settings menu
+    /// </summary>
     public void Settings()
     {
         StartCoroutine(LoadScene("Settings"));
     }
 
-    public void Help()
-    {
-        StartCoroutine(LoadScene("Help"));
-    }
-
-    public void SandBox()
-    {
-        StartCoroutine(LoadScene("SandBox"));
-    }
-
-    public void Modded()
-    {
-        StartCoroutine(LoadScene("ModMenu"));
-    }
-
+    /// <summary>
+    /// loads a scene from name
+    /// </summary>
+    /// <param name="name">the scene to load</param>
     public IEnumerator LoadScene(string name)
     {
+        // load settings
         SettingsControler.Load();
+
+        // show transition
         transition.SetTrigger("Start");
         yield return StartCoroutine(ma.Transition());
+
+        // load the scene
         SceneManager.LoadScene(name);
     }
 
+    /// <summary>
+    /// starts a new game
+    /// </summary>
     public void StartNewGame()
     {
-        // Saves.Reset();
         ContinueGame();
     }
 
+    /// <summary>
+    /// donate button click action
+    /// </summary>
     public void Donate()
     {
         Application.OpenURL("https://ko-fi.com/prestosilver");
     }
 
+    /// <summary>
+    /// discord thing
+    /// </summary>
     public void Discord()
     {
         Application.OpenURL("https://discord.gg/vrVVXktmfV");
     }
 
+    /// <summary>
+    /// clone the github template thing
+    /// </summary>
     public void Clone()
     {
         Application.OpenURL("https://github.com/prestosilver/IP-Mod-Template/generate");
     }
 
+    /// <summary>
+    /// shows the gpgs leaderboards
+    /// </summary>
     public void ShowLeaderboards()
     {
         PlayGamesScript.ShowLeaderboardsUI();
     }
 
+    /// <summary>
+    /// closes the game
+    /// </summary>
     public void Quit()
     {
         Application.Quit();
     }
 
+    /// <summary>
+    /// show the github add mod thing
+    /// </summary>
     public void ShowGHPopup()
     {
         Instantiate(GHPopup, Foreground);
