6b10f2516eb609d91c9406a6e73719ceeb3e81a2
prestosilver <prestosilver.ptp@gmail.com>
2021-10-11 09:10:43 -0400

    comment falling

--- Assets/Scripts/Menu/Falling.cs
+++ Assets/Scripts/Menu/Falling.cs
@@ -4,8 +4,19 @@ using UnityEngine;
 
 public class Falling : GenericController
 {
+    /// <summary>
+    /// the canvas object
+    /// </summary>
     public RectTransform canvas;
+
+    /// <summary>
+    /// the velocity
+    /// </summary>
     public int v;
+
+    /// <summary>
+    /// moves the object
+    /// </summary>
     public void Update()
     {
         transform.localPosition += Vector3.down * v;
