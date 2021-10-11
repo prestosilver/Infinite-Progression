786ec1185d4efb8eb73d38ff062e059598ce9339
prestosilver <prestosilver.ptp@gmail.com>
2021-10-11 09:10:30 -0400

    comment confirm

--- Assets/Scripts/Menu/Confirm.cs
+++ Assets/Scripts/Menu/Confirm.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class Confirm : MonoBehaviour
 {
-    // Start is called before the first frame update
+    /// <summary>
+    /// destroy this
+    /// </summary>
     public void Cancel()
     {
         Destroy(gameObject);
