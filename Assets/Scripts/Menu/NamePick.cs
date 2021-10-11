3d19e43ecf4c0a6af9b81a5eb335b0ae2f69f950
prestosilver <prestosilver.ptp@gmail.com>
2021-10-11 09:21:48 -0400

    comment name picker

--- Assets/Scripts/Menu/NamePick.cs
+++ Assets/Scripts/Menu/NamePick.cs
@@ -6,21 +6,45 @@ using UnityEngine.UI;
 
 public class NamePick : MonoBehaviour
 {
+    /// <summary>
+    /// the current save
+    /// </summary>
     public static Save modPickSave;
+
+    /// <summary>
+    /// save name input
+    /// </summary>
     public InputField nameField;
 
+    /// <summary>
+    /// cancel create save
+    /// </summary>
     public void Cancel()
     {
         Destroy(gameObject);
     }
 
+    /// <summary>
+    /// creates save
+    /// </summary>
     public void Confirm()
     {
+        // make sure name is not empty
+        if (nameField.text == "") return;
+
+        // init save
         Save s = new Save();
+
+        // set name to user input
         s.name = nameField.text;
+
+        // setup save name in saves class
         Saves.saveName = s.name;
+
+        // setup mod picker
         modPickSave = s;
+
+        //load mod picker
         SceneManager.LoadScene("ModPick");
-        Destroy(gameObject);
     }
 }
