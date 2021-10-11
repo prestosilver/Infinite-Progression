a874c060f52b3e2d987a207bc4662d068a2bbec1
prestosilver <prestosilver.ptp@gmail.com>
2021-10-11 09:32:01 -0400

    comment save selector

--- Assets/Scripts/Menu/SaveSelector.cs
+++ Assets/Scripts/Menu/SaveSelector.cs
@@ -7,27 +7,58 @@ using UnityEngine.UI;
 
 public class SaveSelector : MonoBehaviour
 {
+    /// <summary>
+    /// the save file this represents
+    /// </summary>
     public Save save;
+
+    /// <summary>
+    /// the text displaying the name
+    /// </summary>
     public Text nameText;
+
+    /// <summary>
+    /// the delete confirm prefab
+    /// </summary>
     public GameObject confirmPrefab;
 
+    /// <summary>
+    /// loads the save
+    /// </summary>
     public void Load()
     {
+        // setup the save name
         Saves.saveName = save.name;
+
+        // load the mods from the save
         GameController.mods = ModManager.instance.LoadMods(save.mods);
+
+        // load the game
         SceneManager.LoadScene("MainGame");
     }
 
+    /// <summary>
+    /// setup the save name
+    /// </summary>
+    /// <param name="save">the name</param>
     public void setSave(Save save)
     {
         this.save = save;
         nameText.text = save.name;
     }
 
+    /// <summary>
+    /// confirm delete save
+    /// </summary>
     public void deleteSave()
     {
+        // the name of the save
         Saves.saveName = save.name;
+
+        // create confirm dialog
         GameObject go = Instantiate(confirmPrefab);
+
+        // setup delete action
         GameObject confirm = GameObject.FindGameObjectsWithTag("ConfirmButton")[0];
         confirm.GetComponent<Button>().onClick.AddListener(() =>
         {
@@ -37,6 +68,9 @@ public class SaveSelector : MonoBehaviour
         });
     }
 
+    /// <summary>
+    /// shows the save info
+    /// </summary>
     public void showInfo()
     {
         SaveMenuController.instance.ShowInfo(save);
