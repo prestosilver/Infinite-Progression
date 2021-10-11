08577680455e09d182e11b885e82225d127b652e
prestosilver <prestosilver.ptp@gmail.com>
2021-10-11 09:11:08 -0400

    comment menu ani

--- Assets/Scripts/Menu/MenuAnimation.cs
+++ Assets/Scripts/Menu/MenuAnimation.cs
@@ -5,45 +5,78 @@ using UnityEngine.UI;
 
 public class MenuAnimation : MonoBehaviour
 {
+    /// <summary>
+    /// the prefabs for the menu objects
+    /// </summary>
     public List<GameObject> prefabs;
+
+    /// <summary>
+    /// the canvas to spawn the prefabs on
+    /// </summary>
     public RectTransform canvas;
+
+    /// <summary>
+    /// the chance for each prefab
+    /// </summary>
     public List<int> probs;
 
+    /// <summary>
+    /// the total chance
+    /// </summary>
     private int sum;
-    // Start is called before the first frame update
+
+    /// <summary>
+    /// wether the game has been closed
+    /// </summary>
+    public bool quit = false;
+
+    /// <summary>
+    /// setup for spawning and stuff
+    /// </summary>
     public void Start()
     {
+        // setup constant tps to make flash better
         ConsistantTPS.tps = new BigNumber(30);
+
+        // setup sum
         sum = 0;
         foreach (int i in probs)
             sum += i;
 
+        // get the ammount of sliders
         int ammnt = (int)(canvas.rect.width * canvas.rect.height / 9500);
 
-        for (int x = 0; x < ammnt; x++) {
+        // spawn all the sliders
+        for (int x = 0; x < ammnt; x++)
+        {
+            // the slider index thing
             int num = UnityEngine.Random.Range(0, sum);
+
+            // the type of the slider
             int type = 0;
-            foreach (int i in probs) {
+
+            // calculate the slider
+            foreach (int i in probs)
+            {
                 if ((num -= i) < 0)
                     break;
-                type ++;
+                type++;
             }
+
+            // spawn the slider
             GameObject slider = Instantiate(prefabs[type]);
+
+            // add fall to slider
             Falling fall = slider.AddComponent(typeof(Falling)) as Falling;
+
+            // give fall random velocity
             fall.v = Random.Range(3, 7);
             fall.canvas = canvas;
+
+            // set demo mode on the slider
             ((GenericController)slider.GetComponents(typeof(GenericController))[0]).demo = true;
-            // if (slider.GetComponent<DiscountController>()) {
-            //     slider.GetComponent<DiscountController>().demo = true;
-            // } else if (slider.GetComponent<SliderController>()) {
-            //     slider.GetComponent<SliderController>().demo = true;
-            // } else if (slider.GetComponent<MultiplyerController>()) {
-            //     slider.GetComponent<MultiplyerController>().demo = true;
-            // } else if (slider.GetComponent<GeneratorController>()) {
-            //     slider.GetComponent<GeneratorController>().demo = true;
-            // } else if (slider.GetComponent<GeneratorGenController>()) {
-            //     slider.GetComponent<GeneratorGenController>().demo = true;
-            // }
+
+            // move sliders to random position
             int halfwidth = 150 + (int)(canvas.rect.width / 2);
             int halfheight = 150 + (int)(canvas.rect.height / 2);
             slider.transform.parent = this.transform;
@@ -52,12 +85,16 @@ public class MenuAnimation : MonoBehaviour
             slider.transform.localPosition = new Vector3(Random.Range(-halfwidth, halfwidth), Random.Range(-halfheight, halfheight), 0);
             slider.transform.localScale = new Vector3(1, 1, 1);
             slider.transform.rotation = myRotation;
-            //slider.transform.rotation[2] = 0;
         }
     }
 
+    /// <summary>
+    /// transition out of the menu
+    /// </summary>
+    /// <returns></returns>
     public IEnumerator Transition()
     {
+        // move all sliders to the top
         foreach (Falling child in GetComponentsInChildren<Falling>())
         {
             child.v = -40 + Random.Range(3, 5);
@@ -65,23 +102,22 @@ public class MenuAnimation : MonoBehaviour
         yield return Wait(0.9f);
     }
 
-    public bool quit = false;
-
+    /// <summary>
+    /// wait for x ammount of time
+    /// </summary>
+    /// <param name="waitTime"></param>
+    /// <returns></returns>
     IEnumerator Wait(float waitTime)
     {
         float counter = 0;
 
         while (counter < waitTime)
         {
-            //Increment Timer until counter >= waitTime
             counter += Time.deltaTime;
-//             Debug.Log("We have waited for: " + counter + " seconds");
             if (quit)
             {
-                //Quit function
                 yield break;
             }
-            //Wait for a frame so that Unity doesn't freeze
             yield return null;
         }
     }
