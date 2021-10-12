e252815cd0adbc8fece38aa3bd6b015a170fc534
prestosilver <prestosilver.ptp@gmail.com>
2021-10-12 09:33:54 -0400

    comment consistant tps

--- Assets/Scripts/Game/ConsistantTPS.cs
+++ Assets/Scripts/Game/ConsistantTPS.cs
@@ -4,26 +4,50 @@ using UnityEngine;
 
 public class ConsistantTPS : MonoBehaviour
 {
+    /// <summary>
+    /// the currrent tps
+    /// </summary>
     public static BigNumber tps = new BigNumber(120);
+
+    /// <summary>
+    /// the controller attached to this
+    /// </summary>
     private GenericController cont;
+
+    /// <summary>
+    /// a time counter
+    /// </summary>
     private float time = 0;
 
+    /// <summary>
+    /// setup
+    /// </summary>
     public void Start()
     {
         cont = (GenericController)GetComponents(typeof(GenericController))[0];
     }
 
-    // Update is called once per frame
+    /// <summary>
+    /// process ticks
+    /// </summary>
     public void Update()
     {
-        if (tps == new BigNumber(0)) { return; }
+        // avoid div by zero
+        if (tps == new BigNumber(0)) return;
+
+        // increase the timer
         time += Time.deltaTime;
-        if (time < 0.01)
-        {
-            return;
-        }
+
+        // optimization just to limit to a minimum fps
+        if (time < 0.01) return;
+
+        // get the number of ticks to process
         BigNumber ticks = tps * time;
+
+        // process the ticks
         cont.DoTick(ticks);
+
+        // reset time
         time = 0;
     }
 }
